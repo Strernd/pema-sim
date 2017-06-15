956b834dc57485054b219eff862c982cd66a37f5
Bernd Strehl <bernd.strehl@the-white-label.com>
2017-06-15 13:18:46 +0200

    usage explanation

--- CFG.ts
+++ CFG.ts
@@ -1,40 +1,34 @@
 export class CFG{
-    static DRAWING = false;
-    static RUNS = 1000;
+    static DRAWING = true;
 
-    static QUANT_TIMESLOTS = 30;
-    static QUANT_TRUCKS = 20;
-
-    static DELAY_FACTOR = 0.01 * 15;
-
-    static ADOPTION_RATE = 0.5;
+    static RUNS = 1000; // Number of iterations
+    static QUANT_TIMESLOTS = 25; // Number of timeslots
+    static QUANT_TRUCKS = 23; // Number of trucks
+    static DELAY_FACTOR = 0.01 * 15; // Multiplier for the delay (multiplied with total distance)
+    static ADOPTION_RATE = 1; 
     static ENABLE_SWAP = true;
     static ENABLE_PULL = true;
-
     static ENABLE_PUSH = true;
-
-    static TRUCK_DELAY_EXPT = true;
-    // For non exp
+    static TRUCK_DELAY_EXPT = true; // True is exp. dist. - false is norm. dist.
     static TRUCK_DELAY_MU = 0;
     static TRUCK_DELAY_SIGMA = 15;
-    // For exp
     static TRUCK_DELAY_LAMBDA = 1;
-    static LATE_EARLY_DIST = 0.5; // 0.3 => 30% early 70% late
-    // both
-    // static DELAY_FACTOR = 0.01 * 15;
-
-    static TIMESLOT_LEN = 50;
-    static APPEND_EMPTY_TIMESLOTS = 0;
-    static TRUCKS_TOTAL_WAY_MU = 500;
-    static TRUCKS_TOTAL_WAY_SIGMA = 250;
-    static TRUCKS_TOTAL_WAY_MIN = 100;
-    static TRUCK_SAFETY_START_TIME_MIN = 5;
-    static TRUCK_SAFETY_START_TIME_MAX = 15;
-    static TRUCK_DISPATCH_TIME = 40;
+    static LATE_EARLY_DIST = 0.5; // distribution of late and early delays for exp. dist. 0.3 => 30% early 70% late
+    static TIMESLOT_LEN = 50; // length of timeslots
+    static TRUCKS_TOTAL_WAY_MU = 500; // mu parameter for norm. dist. of trucks total way
+    static TRUCKS_TOTAL_WAY_SIGMA = 250; // sigma parameter for norm. dist. of trucks total way
+    static TRUCKS_TOTAL_WAY_MIN = 100; // min way a truck has to have
+    static TRUCK_SAFETY_START_TIME_MIN = 5; // min safety time
+    static TRUCK_SAFETY_START_TIME_MAX = 15; // max safety time
+    static TRUCK_DISPATCH_TIME = 40; // dispatch time for a truck
+    
     static TIME_OFFSET = 200;
-    static SEED = 6;
+
+    static SEED = 7;
     static INDIVIDUAL_ROWS = false;
     static SCALE = 0.8;
+    static APPEND_EMPTY_TIMESLOTS = 0;
+    
 
     static COLORS = {
     GREY: "#9E9E9E",
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Setup
+in order to get a running system you have to clone this repository, then install the package dependcies via `npm install`
+
+Install browserify and the TypeScript Compiler.
+Transpile the .ts files to javascript with `tsc` and then build the dependencies into one single javascript file with `browserify app.js -o -app-build.js`
+
+This step has to be repeated when changes are made.
+
+
+## Usage
+Parameters of the Model can be changed in the CFG.ts
+To run the simulation without graphic representation uncomment the first two lines in the fn() function in app.ts and comment out the rest. Also set the Drawing Parameter to false.
+
+
